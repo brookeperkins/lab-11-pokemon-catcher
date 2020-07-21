1c06567c1cb745a36ad450d06f261b6aa8a1540a
brookeperkins <brookemmperkins@gmail.com>
2020-07-20 19:11:43 -0700

    planning

---
+++ app.js
@@ -0,0 +1,11 @@
+// import functions and grab DOM elements
+import './pokemon.js';
+import './pokemon.js';
+
+export function randomPokemon() {
+    const pokemon = someArray.
+}
+
+// initialize state
+
+// set event listeners to update state and DOM
---
+++ test/app.test.js
@@ -0,0 +1,19 @@
+import '../pokemon.js';
+import '../pokemon.js';
+import { randomPokemon } from '../app.js';
+
+const test = QUnit.test;
+
+test('should return three random pokemon', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = true;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = false;
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
