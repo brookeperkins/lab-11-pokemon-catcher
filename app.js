b08a606557533ae879db52d716a006b04750b9cf
brookeperkins <brookemmperkins@gmail.com>
2020-07-22 08:43:40 -0700

    app.js updates

--- app.js
+++ app.js
@@ -1,73 +1,73 @@
 import pokemonData from './data/pokemon.js';
-import { getRandomPokemon } from './common/utils.js';
+import { getRandomPokemon, findById, encounteredPokemonTotals, caughtPokemonTotals } from './common/utils.js';
 
-const catchButton = document.getElementById('catch-button');
-const radioButtons = document.querySelectorAll('input');
-const labels = document.querySelectorAll('label');
-let randomPokemon0 = getRandomPokemon(pokemonData);
-let randomPokemon1 = getRandomPokemon(pokemonData);
-let randomPokemon2 = getRandomPokemon(pokemonData);
-let caughtArray = [];
+let catches = 0;
+// let caughtArray = [];
 let encounteredArray = [];
 
-// //initialize state
-//for counter: let caughtPokemon = 0; let encounteredPokemon = 0;
+const labels = document.querySelectorAll('label');
+const catchButton = document.querySelector('#catch-button');
+const resultSpan = document.querySelector('#catches-span');
 
-//labels
+function setPage() {
+    let randomPokemon0 = getRandomPokemon(pokemonData);
+    let randomPokemon1 = getRandomPokemon(pokemonData);
+    let randomPokemon2 = getRandomPokemon(pokemonData);
 
-const label0 = labels[0];
-const input0 = label0.children[0];
-const image0 = label0.children[2];
-const span0 = label0.children[2];
+    while (randomPokemon0.id === randomPokemon1.id || randomPokemon0 === randomPokemon2.id || randomPokemon1.id === randomPokemon2.id) {
+        randomPokemon1 = getRandomPokemon(pokemonData);
+        randomPokemon2 = getRandomPokemon(pokemonData);
+    }
 
-input0.value = randomPokemon0.id;
-image0.src = randomPokemon0.url_image;
-span0.textContent = randomPokemon0.pokemon;
-//event listener
+    const label0 = labels[0];
 
-const label1 = labels[1];
-const input1 = label1.children[0];
-const image1 = label1.children[2];
-const span1 = label1.children[2];
+    const input0 = label0.children[0];
+    input0.addEventListener('click', eventHandler());
+    const image0 = label0.children[2];
+    const span0 = label0.children[2];
+    encounteredPokemonTotals(encounteredArray, randomPokemon0.id);
+    input0.value = randomPokemon0.id;
+    image0.src = randomPokemon0.url_image;
+    span0.textContent = randomPokemon0.pokemon;
 
-input1.value = randomPokemon1.id;
-image1.src = randomPokemon1.url_image;
-span1.textContent = randomPokemon1.pokemon;
-//event listener
+    const label1 = labels[1];
 
-const label2 = labels[2];
-const input2 = label2.children[0];
-const image2 = label2.children[2];
-const span2 = label2.children[2];
+    const input1 = label1.children[0];
+    input1.addEventListener('click', eventHandler());
+    const image1 = label1.children[2];
+    const span1 = label1.children[2];
+    input1.value = randomPokemon1.id;
+    image1.src = randomPokemon1.url_image;
+    span1.textContent = randomPokemon1.pokemon;
 
-input2.value = randomPokemon2.id;
-image2.src = randomPokemon2.url_image;
-span2.textContent = randomPokemon2.pokemon;
-//event listener
+    const label2 = labels[2];
 
-while (randomPokemon0.id === randomPokemon1.id || randomPokemon0 === randomPokemon2.id || randomPokemon1.id === randomPokemon2.id) {
-    randomPokemon1 = getRandomPokemon(pokemonData);
-    randomPokemon2 = getRandomPokemon(pokemonData);
+    const input2 = label2.children[0];
+    input2.addEventListener('click', eventHandler());
+    const image2 = label2.children[2];
+    const span2 = label2.children[2];
+    input2.value = randomPokemon2.id;
+    image2.src = randomPokemon2.url_image;
+    span2.textContent = randomPokemon2.pokemon;
+
 }
-//add to encountered array and caught array
 
-//add pokemon properties to html elements
-input0.value = randomPokemon0.id;
-image0.src = randomPokemon0.url_image;
+function eventHandler() {
+    catches++;
 
-input1.value = randomPokemon1.id;
-image1.src = randomPokemon1.url_image;
+    const clickedPokemon = document.querySelector('input:checked');
+    const pokeBall = clickedPokemon.value;
+    const caughtPokemon = findById(pokemonData, pokeBall);
 
-input2.value = randomPokemon2.id;
-image2.src = randomPokemon2.url_image;
+    caughtPokemonTotals(encounteredArray, caughtPokemon);
+    resultSpan.textContent = 'Caught a wild ' + caughtPokemon.pokemon;
+    // console.log(catches);
+}
 
-
-// radioButtons.forEach((radioPokemon, i) => {
-//     if (i === 0) {
-//         radioPokemon.value = randomPokemon0.id;
-//     } else if (i === 1) {
-//         radioPokemon.value = randomPokemon1.id;
-//     } else if (i === 2) {
-//         radioPokemon.value = randomPokemon2.id;
-//     }
-// });
+if (catches < 10){
+    catchButton.addEventListener('click', setPage);
+} else {
+    catchButton.addEventListener('click', () => {
+        window.location.href = './results.html';
+    });
+}
