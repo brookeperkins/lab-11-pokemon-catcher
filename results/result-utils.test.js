00ac6cadc1a0d254ab9ec6b324083e4df74dc95a
brookeperkins <brookemmperkins@gmail.com>
2020-07-23 21:32:59 -0700

    eslint again

--- results/result-utils.test.js
+++ results/result-utils.test.js
@@ -1,19 +1,17 @@
-import {} from '../results/results-utils';
-
 const test = QUnit.test;
 
 test('should return three random pokemon', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = false;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = false;
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.deepEqual(actual, expected);
+    expect.equal(actual, expected);
 });
 
 //refactoring: rename and organize files and pathways in a way that makes sense
